1d90a904e452c17b78a5706c1ea36ebda79aa480
Georgi Stefanov <georgistephanov@gmail.com>
2017-11-30 19:23:43 +0000

    Rotated the progress bar to the desired position

--- app/src/main/java/com/georgistephanov/android/pomodorotimer/MainActivity.java
+++ app/src/main/java/com/georgistephanov/android/pomodorotimer/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+	// Task length in milliseconds
+	private static final int TASK_LENGTH_MS = 1500000;
 	ProgressBar progressBarTimer;
 
 	@Override
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
 		progressBarTimer = findViewById(R.id.pb_timer);
 		ObjectAnimator animation = ObjectAnimator.ofInt(progressBarTimer, "progress", 500, 0);
-		animation.setDuration(10000);
+		animation.setDuration(TASK_LENGTH_MS);
 		animation.setInterpolator(new DecelerateInterpolator());
-		//animation.start();
+		animation.start();
 	}
 }
