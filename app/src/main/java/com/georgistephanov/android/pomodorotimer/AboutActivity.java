9bbaf3ab28a4e715556771259158a55c8a8c3704
Georgi Stefanov <georgistephanov@gmail.com>
2017-12-07 08:54:14 +0000

    Populated the about activity list view

--- app/src/main/java/com/georgistephanov/android/pomodorotimer/AboutActivity.java
+++ app/src/main/java/com/georgistephanov/android/pomodorotimer/AboutActivity.java
@@ -1,15 +1,13 @@
 package com.georgistephanov.android.pomodorotimer;
 
-import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,8 +16,12 @@ import android.widget.TextView;
  */
 
 public class AboutActivity extends ListActivity {
-	private static final String [] items = {
+	private static final String [] aboutNames = {
 			"Version", "Credits", "Open Source Licenses"
+	};
+
+	private static final String [] aboutLabels = {
+			"1.0", "", ""
 	};
 
 	@Override
@@ -27,14 +29,47 @@ public class AboutActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 
-		setListAdapter(new ArrayAdapter<String>(
-				this,
-				R.layout.about_list_adapter,
-				items ));
+		// Populate the list
+		setListAdapter(new CustomAdapter(this));
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
+	}
+
+	private class CustomAdapter extends ArrayAdapter<String> {
+		Context context;
+
+		CustomAdapter(Context context) {
+			super(AboutActivity.this, R.layout.about_list_adapter, R.id.aboutName, aboutNames);
+			this.context = context;
+		}
+
+		@NonNull
+		@Override
+		public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+			View row = super.getView(position, convertView, parent);
+
+			// If there is an about label existing and it is not empty for the current row -> display it
+			if (position < aboutLabels.length && aboutLabels[position].length() != 0) {
+
+				// Remove the bottom padding of the name
+				int padding = (int) context.getResources().getDisplayMetrics().density * 15;
+				((TextView) row.findViewById(R.id.aboutName)).setPadding(
+						padding,
+						padding,
+						padding,
+						0
+				);
+
+				// Set the label text and its visibility
+				TextView aboutLabel = row.findViewById(R.id.aboutLabel);
+				aboutLabel.setText(context.getResources().getString(R.string.plain_string, "1.0"));
+				aboutLabel.setVisibility(View.VISIBLE);
+			}
+
+			return row;
+		}
 	}
 }
